302362adfad08b8fa567caf87ab68353bf30be46
leafeon-b <grad.f.ishida@gmail.com>
2022-05-12 12:04:37 +0000

    feat:コマンドの編集/追加

--- src/main/main.py
+++ src/main/main.py
@@ -2,7 +2,6 @@
 import random
 
 import disnake
-from disnake import TextInputStyle
 from disnake.ext import commands
 
 import settings
@@ -35,19 +34,20 @@ async def denpo(inter: disnake.AppCmdInter):
 
 
 @bot.slash_command()
-async def shuffle(inter: disnake.AppCmdInter):
-    """shuffle vc members"""
-    vc: disnake.VoiceChannel = disnake.utils.get(inter.guild.voice_channels, name="General")
+async def shuffle(inter: disnake.AppCmdInter, vc: disnake.VoiceChannel = None):
+    """Shuffle vc members(default vc is "General")."""
+    if vc is None:
+        vc = disnake.utils.get(inter.guild.voice_channels, name="General")
     member_names = [member.name for member in vc.members]
-    # print(f"{member_names=}")
     member_names.append("hoge")
     member_names.append("fuga")
     random.shuffle(member_names)
     await inter.response.send_message("\n".join(member_names))
 
 
-@bot.slash_command(name="char", description="Creates random alphabet.")
+@bot.slash_command(name="char")
 async def random_character(inter: disnake.AppCmdInter):
+    """Creates random alphabet."""
     characters = ["あ","い","う","え","お","か","き","く","け","こ","さ","し","す","せ","そ","た","ち","つ","て","と","な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ","ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ","わ","を","ん"]
     c = random.choice(characters)
     await inter.response.send_message(c)
@@ -57,6 +57,18 @@ async def random_character(inter: disnake.AppCmdInter):
 async def random_n(inter: disnake.AppCmdInter):
     n = random.randint(1, 100)
     await inter.response.send_message(n, ephemeral=True)
+
+
+@bot.slash_command()
+async def dice(inter: disnake.AppCmdInter, number: commands.Range[1, ...]):
+    """Show the result of dice roll only to you."""
+    await inter.response.send_message(random.randint(1, number), ephemeral=True)
+
+
+@bot.slash_command()
+async def d(inter: disnake.AppCmdInter, number: commands.Range[1, ...]):
+    """Alias of dice."""
+    await dice(inter, number)
 
 
 bot.run(settings.TOKEN)
